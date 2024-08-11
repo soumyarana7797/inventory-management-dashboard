cd8b30441832a9b60db4d24e93c4aba0e9bb3771
soumyarana7797 <soumya.rana9999@gmail.com>
2024-08-11 20:02:37 +0530

    sidebar & navbar collapsed

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -1,36 +1,61 @@
-"use client"
-import { Bell, Menu, Settings } from 'lucide-react'
-import Link from 'next/link'
-import React from 'react'
+"use client";
 
-type Props = {}
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { setIsDarkMode, setIsSidebarCollapsed } from "@/state";
+import { Bell, Menu, Moon, Settings, Sun } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Navbar = () => {
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  const toggleSidebar = () => {
+    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+  };
+
+  const toggleDarkMode = () => {
+    dispatch(setIsDarkMode(!isDarkMode));
+  };
+
   return (
     <div className="flex justify-between items-center w-full mb-7">
-        {/* LEFT SIDE */}
-        <div className="flex justify-between items-center gap-5">
-            <button className=" px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
-            onClick={() => {}}
-            >
-                <Menu className="w-4 h-4" />
-            </button>
-        </div>
+      {/* LEFT SIDE */}
+      <div className="flex justify-between items-center gap-5">
+        <button
+          className="px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
+          onClick={toggleSidebar}
+        >
+          <Menu className="w-4 h-4" />
+        </button>
+
         <div className="relative">
-        <input
+          <input
             type="search"
             placeholder="Start type to search groups & products"
             className="pl-10 pr-4 py-2 w-50 md:w-60 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500"
           />
-            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-non">
+
+          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-non">
             <Bell className="text-gray-500" size={20} />
           </div>
         </div>
-        {/* RIGHT SIDE  */}
-        <div className="flex justify-between items-center gap-5">
+      </div>
+
+      {/* RIGHT SIDE */}
+      <div className="flex justify-between items-center gap-5">
         <div className="hidden md:flex justify-between items-center gap-5">
           <div>
-            <button onClick={() => {}}>
+            <button onClick={toggleDarkMode}>
+              {isDarkMode ? (
+                <Sun className="cursor-pointer text-gray-500" size={24} />
+              ) : (
+                <Moon className="cursor-pointer text-gray-500" size={24} />
+              )}
             </button>
           </div>
           <div className="relative">
@@ -41,16 +66,22 @@ const Navbar = () => {
           </div>
           <hr className="w-0 h-7 border border-solid border-l border-gray-300 mx-3" />
           <div className="flex items-center gap-3 cursor-pointer">
-              Image
+            <Image
+              src=""
+              alt="Profile"
+              width={50}
+              height={50}
+              className="rounded-full h-full object-cover"
+            />
             <span className="font-semibold">Ed Roh</span>
           </div>
         </div>
-        <Link href="/">
+        <Link href="/settings">
           <Settings className="cursor-pointer text-gray-500" size={24} />
         </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { useEffect } from 'react'
 import Navbar from './(components)/Navbar'
 import Sidebar from './(components)/Sidebar'
